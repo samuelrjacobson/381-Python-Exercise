a241ad6e97a73362158be41f1ba79b93ac85af72
samuelrjacobson <132033453+samuelrjacobson@users.noreply.github.com>
2023-11-01 23:46:47 +0000

    fix: add menu with option to quit
    Added a menu to display options of functions to perform on data. So far, the menu only has one option--enter q to quit. Any other input will result in the menu getting displayed again.
    
    issue #1

--- query.py
+++ query.py
@@ -1,3 +1,16 @@
 from movies import Movies
 
 movies = Movies('./movies.txt')
+
+request = ""
+while(request != "q"):
+    print("q: quit");
+    #print("sn: search movie names");
+    #print("sc: search cast");
+    #print("list: print all the movie names");
+
+    request = input()
+
+    #if(request != "q"):
+    #    print("Do something")
+    
