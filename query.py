6822c1aac995dd2205a151c72b6afa0eab896679
samuelrjacobson <132033453+samuelrjacobson@users.noreply.github.com>
2023-11-02 02:26:54 +0000

    fix: add option to search for movies names
    Added an option 'sn' to menu. User may choose this option, then enter a search term. All movie names containing that string (case-insensitive) will be displayed.
    
    issue #3

--- query.py
+++ query.py
@@ -2,16 +2,25 @@ from movies import Movies
 
 movies = Movies('./movies.txt')
 
+# Keep displaying menu till user enters q to quit
 request = ""
 while(request != "q"):
-    print("q: quit");
-    #print("sn: search movie names");
+    print("\nq: quit");
+    print("sn: search movie names");
     #print("sc: search cast");
     print("list: print all the movie names");
 
     request = input()
 
+    # Display a list of all movie names
     if(request == "list"):
         for m in movies._movies:
             print(m["name"])
-    
+
+    # Return results for search by movie name
+    elif(request == "sn"):
+        print("Enter a search term: ")
+        word = input()
+        for m in movies._movies:
+            if(word.lower() in m["name"].lower()):
+                print(m["name"])
