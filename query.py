0382dc68258efff320346cfede62eb187116dd55
samuelrjacobson <132033453+samuelrjacobson@users.noreply.github.com>
2023-11-02 03:18:27 +0000

    fix: add option to search by cast names
    Added 'sc' option to menu. When selected, will prompt for search term. Then, for every movie starring an actor whose name contains that string, display the movie name and all actors in that movie whose names contain that string.
    
    issue #4

--- query.py
+++ query.py
@@ -3,14 +3,14 @@ from movies import Movies
 movies = Movies('./movies.txt')
 
 # Keep displaying menu till user enters q to quit
-request = ""
+request = "";
 while(request != "q"):
     print("\nq: quit");
     print("sn: search movie names");
-    #print("sc: search cast");
+    print("sc: search cast");
     print("list: print all the movie names");
 
-    request = input()
+    request = input();
 
     # Display a list of all movie names
     if(request == "list"):
@@ -20,7 +20,22 @@ while(request != "q"):
     # Return results for search by movie name
     elif(request == "sn"):
         print("Enter a search term: ")
-        word = input()
+        word = input();
         for m in movies._movies:
             if(word.lower() in m["name"].lower()):
                 print(m["name"])
+
+    # Return results for search by cast names
+    elif(request == "sc"):
+        print("Enter a search term: ")
+        word = input();
+        for m in movies._movies:
+            matches = [];
+            for c in m["cast"]:
+                if(word.lower() in c.lower()):
+                    matches.append(c);
+            if(len(matches) > 0):
+                print(m["name"]);
+                print("['", end="");
+                print(*matches, sep = "' , '", end="");
+                print("']");
